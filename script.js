89a1b3e3adeeadf5be675b268dad1c2d4ddac971
M Ihsanul Afkar <ihsanul2001@gmail.com>
2021-09-10 20:09:47 +0700

    try getElementsByTagName

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 const judul = document.getElementById('judul');
 judul.style.color = 'red';
 judul.style.backgroundColor = 'grey';
+judul.innerHTML = 'Halo Dunia!';
+
+// document.getElementsByTagName() -> HTMLCollection
+const p = document.getElementsByTagName('p');
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'lightblue';
+}
